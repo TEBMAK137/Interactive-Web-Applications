08a4cf104e4ea94dd9b071fd9814473999b2a011
Tebogo Makgakga <makgakgatg@gmail.com>
2023-04-23 08:25:38 +0200

    Updates

--- Interactive-Web-Applications/IWA1/scripts.js
+++ Interactive-Web-Applications/IWA1/scripts.js
@@ -6,8 +6,6 @@ const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
 
-
-
 const subtractHandler = () => {
   const newValue = parseInt(number.value) -1
   number.value = newValue
